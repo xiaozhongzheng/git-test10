ec3ba297706cfcac2712b97642bf4139bde30781
xiaogit <xiao@aa.com>
2023-11-30 09:39:07 +0800

    package com.xzz.git;
    public class GitDemo {
        public static void main(String[] args) {
    
            System.out.println("Hello git!");
            System.out.println("Hello git01!");
            System.out.println("Hello git02");
            System.out.println("master commit");
            System.out.println("hot-fix commit");
            System.out.println("push code");
        }
    }

--- src/main/java/com/xzz/git/GitDemo.java
+++ src/main/java/com/xzz/git/GitDemo.java
@@ -8,5 +8,6 @@ public class GitDemo {
         System.out.println("Hello git02");
         System.out.println("master commit");
         System.out.println("hot-fix commit");
+        System.out.println("push code");
     }
 }
